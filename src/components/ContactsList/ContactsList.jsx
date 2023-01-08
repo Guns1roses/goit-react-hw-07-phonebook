66e27d4a3dfee4b7323bf23dee84ce21e7fe1b06
Andrii Stepaniuk <andrewvstepanyuk@gmail.com>
2023-01-08 11:24:28 +0200

    add redux

---
+++ src/components/ContactsList/ContactsList.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { ContactItem } from 'components';
+import { Empty, Title, Wrapper } from './ContactsList.styled';
+
+const ContactsList = ({ contacts }) => {
+  return (
+    <Wrapper>
+      <Title>Contacts</Title>
+      {contacts.length > 0 ? (
+        <ul>
+          {contacts.map(({ id, name, number }) => (
+            <ContactItem key={id} id={id} name={name} number={number} />
+          ))}
+        </ul>
+      ) : (
+        <Empty>Contact list is empty</Empty>
+      )}
+    </Wrapper>
+  );
+};
+
+export default ContactsList;
+
+ContactsList.propTypes = {
+  contacts: PropTypes.arrayOf(PropTypes.object),
+  // deleteHandler: PropTypes.func,
+};
+
+// const ContactsList = ({ contacts, deleteHandler }) => {
+//   return (
+//     <Wrapper>
+//       <Title>Contacts</Title>
+//       {contacts.length > 0 ? (
+//         <ul>
+//           {contacts.map(({ id, name, number }) => (
+//             <ContactItem
+//               key={id}
+//               id={id}
+//               name={name}
+//               number={number}
+//               deleteHandler={deleteHandler}
+//             />
+//           ))}
+//         </ul>
+//       ) : (
+//         <Empty>Contact list is empty</Empty>
+//       )}
+//     </Wrapper>
+//   );
+// };
+
